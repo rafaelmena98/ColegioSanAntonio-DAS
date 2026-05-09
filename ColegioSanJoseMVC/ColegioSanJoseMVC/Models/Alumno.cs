93e330e5f57b9e02752a2dca950baf60f331aa37
rafaelmena98 <rafaelmenamejia98@gmail.com>
2026-05-09 01:08:02 -0600

    alumnos.cs

---
+++ ColegioSanJoseMVC/ColegioSanJoseMVC/Models/Alumno.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ColegioSanJoseMVC.Models
+{
+    public class Alumno
+    {
+
+        public int AlumnoId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength (100)]
+        public string Apellido { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Grado { get; set; }
+
+        public ICollection<Expediente>? Expedientes { get; set; }
+    }
+}
