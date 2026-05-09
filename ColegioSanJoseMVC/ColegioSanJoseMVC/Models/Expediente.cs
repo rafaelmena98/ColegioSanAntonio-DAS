ea89af74e047d2c9af5e961a437199b90b5b7ed7
rafaelmena98 <rafaelmenamejia98@gmail.com>
2026-05-09 00:30:31 -0600

    Expediente, Materia

---
+++ ColegioSanJoseMVC/ColegioSanJoseMVC/Models/Expediente.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ColegioSanJoseMVC.Models
+{
+    public class Expediente
+    {
+        public int ExpedienteId { get; set; }
+
+        [Required]
+        public int AlumnoId { get; set; }
+
+        [Required]
+        public int MateriaId { get; set; }
+
+        [Range(0, 10)]
+        [Column(TypeName = "decimal(5,2)")]
+        public decimal? NotaFinal { get; set; }
+
+        [StringLength(500)]
+        public string? Observaciones { get; set; }
+
+        public Alumno? Alumno { get; set; }
+
+        public Materia? Materia { get; set; }
+
+    }
+}
---
+++ ColegioSanJoseMVC/ColegioSanJoseMVC/Models/Materia.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ColegioSanJoseMVC.Models
+{
+    public class Materia
+    {
+        public int MateriaId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string NombreMateria { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Docente { get; set; }
+
+        public ICollection<Expediente>? Expedientes { get; set; }
+    }
+}
